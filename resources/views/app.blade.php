4e71fa01d43bf213706dc4c318dd95c9b2ea07f9
cairofelipedev <cairofelipedev@gmail.com>
2025-02-14 10:35:02 -0300

    Add tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -24,7 +24,23 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <!-- Scripts -->
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-PD7WNW7C');
+    </script>
     @routes
     @viteReactRefresh
     @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
@@ -32,6 +48,11 @@
 </head>
 
 <body class="font-sans antialiased">
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PD7WNW7C"
+            height="0" width="0" style="display:none;visibility:hidden">
+        </iframe>
+    </noscript>
     @inertia
 </body>
 
