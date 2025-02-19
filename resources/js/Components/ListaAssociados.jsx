e24dd771a69fcd82018fff9619565409a2ca4903
Cairo <cairofelipedev@gmail.com>
2025-02-19 01:48:37 -0300

    images add

--- resources/js/Components/ListaAssociados.jsx
+++ resources/js/Components/ListaAssociados.jsx
@@ -2,24 +2,66 @@ import React, { useState } from "react";
 
 // Dados dos associados
 const associados = [
-  { nome: "CHANGE TURISMO", categoria: "agencia-de-viagem" },
-  { nome: "NAJA TURISMO", categoria: "agencia-de-viagem" },
-  { nome: "CAPUCHINO PRESS", categoria: "assessoria-de-imprensa" },
-  { nome: "CS7 MKT DIGITAL", categoria: "agencia-de-marketing-digital" },
-  { nome: "AGÊNCIA ZEAL", categoria: "agencia-de-marketing-digital" },
-  { nome: "LA MAISON", categoria: "buffet" },
-  { nome: "ILMAR GOURMET", categoria: "buffet" },
-  { nome: "NORMA ZELIA", categoria: "mestre-de-cerimonia" },
-  { nome: "MILENA GADELHA", categoria: "mestre-de-cerimonia" },
-  { nome: "A&M PRODUÇÕES ARTÍSTICAS", categoria: "montagem" },
-  { nome: "ARTE PRODUÇÕES", categoria: "montagem" },
-  { nome: "BRILHANTE EVENTOS", categoria: "montagem" },
-  { nome: "MONTADORA EXPO PRODUÇÕES", categoria: "montagem" },
-  { nome: "LUMINART", categoria: "montagem" },
-  { nome: "PHOENIX SOLUÇÕES", categoria: "montagem" },
-  { nome: "FIRMA PRODUÇÕES", categoria: "montagem" },
-  { nome: "ITI - INSTITUTO DE TRADUÇÃO E INTERPRETAÇÃO", categoria: "traducao-simultanea" },
-  { nome: "GRUPO PERES", categoria: "seguranca-para-eventos" },
+  // Agência de Viagem
+  { nome: "CHANGE TURISMO", categoria: "agencia-de-viagem", imagem: "CHANGE TURISMO.jpg" },
+  { nome: "NAJA TURISMO", categoria: "agencia-de-viagem", imagem: "NAJA TURISMO.png" },
+
+  // Assessoria de Imprensa
+  { nome: "CAPUCHINO PRESS", categoria: "assessoria-de-imprensa", imagem: "CAPUCHINO.png" },
+
+  // Agência de Marketing Digital
+  { nome: "CS7 MKT DIGITAL", categoria: "agencia-de-marketing-digital", imagem: "CS7MKT.png" },
+  { nome: "AGÊNCIA ZEAL", categoria: "agencia-de-marketing-digital", imagem: "AGENCIA ZEAL.png" },
+
+  // Buffet
+  { nome: "LA MAISON", categoria: "buffet", imagem: "LA MAISON.png" },
+  { nome: "ILMAR GOURMET", categoria: "buffet", imagem: "ILMAR GOURMET.png" },
+
+  // Mestre de Cerimônia
+  { nome: "NORMA ZELIA", categoria: "mestre-de-cerimonia", imagem: "LOGO FIRMA - Administrativo Firma.png" },
+  { nome: "MILENA GADELHA", categoria: "mestre-de-cerimonia", imagem: "logo_milena_preta - Milena Gadelha Voz.png" },
+
+  // Montagem
+  { nome: "A&M PRODUÇÕES ARTÍSTICAS", categoria: "montagem", imagem: "A&M.jpg" },
+  { nome: "ARTE PRODUÇÕES", categoria: "montagem", imagem: "ARTE PRODUÇÕES.jpeg" },
+  { nome: "BRILHANTE EVENTOS", categoria: "montagem", imagem: "BRILHANTE EVENTOS.jpg" },
+  { nome: "MONTADORA EXPO PRODUÇÕES", categoria: "montagem", imagem: "MONTADORAEXPO.png" },
+  { nome: "LUMINART", categoria: "montagem", imagem: "LUMINART.png" },
+  { nome: "PHOENIX SOLUÇÕES", categoria: "montagem", imagem: "PHOENIX.png" },
+  { nome: "FIRMA PRODUÇÕES", categoria: "montagem", imagem: "FIRMA.png" },
+
+  // Tradução Simultânea
+  { nome: "ITI - INSTITUTO DE TRADUÇÃO E INTERPRETAÇÃO", categoria: "traducao-simultanea", imagem: "I.T.I..jpeg" },
+
+  // Segurança para Eventos
+  { nome: "GRUPO PERES", categoria: "seguranca-para-eventos", imagem: "PEREZ.jpg" },
+
+  // Restaurantes
+  { nome: "COLOSSO FORTALEZA", categoria: "restaurantes", imagem: "logo-colosso FORTALEZA.png" },
+  { nome: "CERVEJARIA TURATTI", categoria: "restaurantes", imagem: "TURATTI.png" },
+  { nome: "ILLA MARE", categoria: "restaurantes", imagem: "ILLA MARE.png" },
+  { nome: "BOTECO DO ILLA", categoria: "restaurantes", imagem: "BOTECO DO ILLA.png" },
+  { nome: "BOTECO DONA MARIA", categoria: "restaurantes", imagem: "BOTECO DONA MARIA.png" },
+  { nome: "COMPLEXO CROCOBEACH", categoria: "restaurantes", imagem: "CROCOBEACH.png" },
+  { nome: "GUARDERIA BRASIL", categoria: "restaurantes", imagem: "GUARDERIA BRASIL.jpeg" },
+  { nome: "CHICO DO CARANGUEJO PRAIA DO FUTURO", categoria: "restaurantes", imagem: "chico.png" },
+
+  // Organizadora de Eventos
+  { nome: "ARX EVENTOS", categoria: "organizadora-de-eventos", imagem: "Arx.png" },
+  { nome: "E + EVENTOS", categoria: "organizadora-de-eventos", imagem: "E.png" },
+  { nome: "ALAS EVENTOS", categoria: "organizadora-de-eventos", imagem: "ALAS.png" },
+  { nome: "KANGURU PROMOÇÕES", categoria: "organizadora-de-eventos", imagem: "KANGURU PRODUÇÕES.png" },
+  { nome: "KAKTUS EVENTOS", categoria: "organizadora-de-eventos", imagem: "KAKTUS.png" },
+  { nome: "MAESTRIA COMUNICAÇÃO E EVENTOS", categoria: "organizadora-de-eventos", imagem: "MAESTRIA.png" },
+  { nome: "PRÁTICA EVENTOS", categoria: "organizadora-de-eventos", imagem: "PRATICA.png" },
+  { nome: "THAIS SOMBRA EVENTOS CORPORATIVOS", categoria: "organizadora-de-eventos", imagem: "THAIS SOMBRA.png" },
+
+  // Receptivo e Transporte
+  { nome: "VM TURISMO", categoria: "receptivo-e-transporte", imagem: "VM TURISMO.png" },
+  { nome: "VITORINO TURISMO", categoria: "receptivo-e-transporte", imagem: "VITORINO TURISMO.png" },
+
+  // Limpeza Geral
+  { nome: "GRUPO INOVAR", categoria: "limpeza-geral", imagem: "INOVAR.jpg" },
 ];
 
 const categoriasTraduzidas = {
@@ -31,6 +73,10 @@ const categoriasTraduzidas = {
   "montagem": "Montagem",
   "traducao-simultanea": "Tradução Simultânea",
   "seguranca-para-eventos": "Segurança para Eventos",
+  "restaurantes": "Restaurantes",
+  "organizadora-de-eventos": "Organizadora de Eventos",
+  "receptivo-e-transporte": "Receptivo e Transporte",
+  "limpeza-geral": "Limpeza Geral",
 };
 
 const ListaAssociados = () => {
@@ -52,7 +98,7 @@ const ListaAssociados = () => {
       </p>
 
       {/* Filtros */}
-      <div className="flex flex-col md:flex-row justify-center items-center gap-4 mb-8 p-4 bg-gray-600 rounded-full shadow-lg mx-auto max-w-xl">
+      <div className="flex flex-col md:flex-row justify-center items-center gap-4 mb-8 p-4 bg-gray-800 rounded-full shadow-lg mx-auto max-w-xl">
         <select
           value={categoriaSelecionada}
           onChange={(e) => setCategoriaSelecionada(e.target.value)}
@@ -84,9 +130,10 @@ const ListaAssociados = () => {
               className="p-5 bg-white rounded-xl shadow-md hover:shadow-lg transition-shadow flex flex-col items-center text-center"
             >
               <img
-                src="https://via.placeholder.com/150"
+                src={`./images/associados/${associado.imagem}`}
                 alt="Associado"
-                className="w-24 h-24 rounded-full mb-4"
+                className="w-full h-72 mb-4"
+                loading="lazy"
               />
               <h3 className="text-xl font-semibold text-gray-800">{associado.nome}</h3>
               <p className="text-gray-600 text-sm">
