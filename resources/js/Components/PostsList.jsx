12cb414eb193fd21557df1f05d72e692fa11071b
AleexBrendon <alexbrendo.dev@gmail.com>
2025-04-30 12:09:29 -0300

    ajust impressa

--- resources/js/Components/PostsList.jsx
+++ resources/js/Components/PostsList.jsx
@@ -1,41 +1,70 @@
 import { useEffect, useState } from "react";
 import { Link } from "@inertiajs/react";
 import axios from "axios";
+import style from "@/Components/Search/videoArea/videoArea.module.scss";
 
 export default function PostsList() {
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        axios.get("/api/posts")
-            .then(response => setPosts(response.data))
-            .catch(error => console.error("Erro ao carregar posts:", error));
+        axios
+            .get("/api/posts")
+            .then((response) => setPosts(response.data))
+            .catch((error) => console.error("Erro ao carregar posts:", error));
     }, []);
 
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 pt-28 px-10">
-            {posts.map(post => (
-                <div key={post.id} className="bg-white shadow-lg rounded-lg overflow-hidden">
-                    <img
-                        src={`./uploads/${post.image}`}
-                        alt={post.title}
-                        className="w-full h-48 object-cover"
-                    />
-                    <div className="p-4 flex justify-between">
-                        <div>
-                            <h2 className="text-lg font-semibold">{post.title}</h2>
-                            <p className="text-gray-500 text-sm">
-                                {new Date(post.published_at).toLocaleDateString("pt-BR")}
-                            </p>
+        <div>
+            <div className={style.infoArea2}>
+                <h1 className="uppercase tracking-widest font-raleway font-black text-[#0C9C95] py-3 lg:text-6xl text-4xl pt-28 px-10">
+                    Impressa
+                </h1>
+                <p className={`${style.description} px-12 text-lg`}>
+                    Espaço para artigos exclusivos escritos pela Assessoria e
+                    imprensa: Capuccino, com análises de mercado e novidades.
+                    <br/>
+                    Cobertura detalhada de eventos e iniciativas promovidas pelo
+                    Visite Ceará.
+                    <br/><br/>
+                    ●Próximos Eventos (calendário):
+                    Acompanhe uma lista sempre atualizada dos eventos futuros em Fortaleza, com detalhes sobre datas, locais e programação.
+                    <br/>
+                    ● Cases de Sucesso:
+                     Aqui você pode encontrar informações sobre  eventos que foram Cases de sucesso em nosso destino.
+                </p>
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 pt-5 px-10">
+                {posts.map((post) => (
+                    <div
+                        key={post.id}
+                        className="bg-white shadow-lg rounded-lg overflow-hidden"
+                    >
+                        <img
+                            src={`./uploads/${post.image}`}
+                            alt={post.title}
+                            className="w-full h-48 object-cover"
+                        />
+                        <div className="p-4 flex justify-between">
+                            <div>
+                                <h2 className="text-lg font-semibold">
+                                    {post.title}
+                                </h2>
+                                <p className="text-gray-500 text-sm">
+                                    {new Date(
+                                        post.published_at
+                                    ).toLocaleDateString("pt-BR")}
+                                </p>
+                            </div>
+                            <Link
+                                href={`/post/${post.slug}`}
+                                className="mt-3 inline-block px-4 py-2 bg-orange-600 text-white rounded-full hover:bg-blue-700"
+                            >
+                                Ver mais
+                            </Link>
                         </div>
-                        <Link
-                            href={`/post/${post.slug}`}
-                            className="mt-3 inline-block px-4 py-2 bg-orange-600 text-white rounded-full hover:bg-blue-700"
-                        >
-                            Ver mais
-                        </Link>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     );
 }
