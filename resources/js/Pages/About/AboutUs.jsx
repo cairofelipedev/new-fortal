6622cf7334b19c426c234041fd13d17b32f4b305
cairofelipedev <cairofelipedev@gmail.com>
2025-04-30 11:16:53 -0300

    New pages dinamics for productions

--- resources/js/Pages/About/AboutUs.jsx
+++ resources/js/Pages/About/AboutUs.jsx
@@ -1,51 +1,16 @@
 import Footer from "@/Components/Footer";
 import NavBar from "@/Components/Home/NavBar/Navbar";
 import { Head } from "@inertiajs/react";
+import BannerDestak from '@/Components/Home/bannerDestak/bannerDestak';
 
 export default function Welcome() {
     return (
         <>
-            <Head title="Porquê Fortaleza?" />
+            <Head title="Quem Somos" />
 
             <NavBar />
 
-            <h1 className="uppercase tracking-widest font-raleway font-black text-[#0C9C95] py-3 lg:text-4xl text-4xl pt-32 px-10">
-                Quem Somos
-            </h1>
-
-            <p className="text-gray-600 text-justify lg:text-lg text-lg pb-5 px-12">
-                O Fortaleza Convention & Visitors Bureau / Visite Ceará foi
-                fundado em 1996 e é uma entidade privada sem fins lucrativos,
-                congregando diversos associados, que representam todos os
-                segmentos ligados à atividade turística. Manter relacionamento
-                com todos os segmentos econômicos que, direta ou indiretamente,
-                se beneficiam do turismo. A Instituição tem como objetivo
-                ampliar o número de visitantes no destino e, consequentemente,
-                aumentar o volume de negócios e o mercado de consumo, por meio
-                da cadeia produtiva do turismo. As atividades são realizadas por
-                meio de ações de promoção, capacitação profissional, captação,
-                geração e incremento de eventos e negócios.
-            </p>
-            <section className="grid grid-cols-2 px-3">
-                <div>
-                    <h1 className="uppercase tracking-widest font-raleway font-black text-[#0C9C95] py-3 lg:text-2xl text-4xl pt-32 px-10">
-                        Conselho
-                    </h1>
-
-                    <p className="text-gray-600 text-justify lg:text-lg text-lg pb-5 px-12">
-                        Lista
-                    </p>
-                </div>
-                <div>
-                    <h1 className="uppercase tracking-widest font-raleway font-black text-[#0C9C95] py-3 lg:text-2xl text-4xl pt-32 px-10">
-                        Equipe
-                    </h1>
-
-                    <p className="text-gray-600 text-justify lg:text-lg text-lg pb-5 px-12">
-                        Lista
-                    </p>
-                </div>
-            </section>
+            <BannerDestak pagina="QUEMSOMOS" posicao={1} />
 
             <Footer />
         </>
--- resources/js/Pages/Associase/IndexAssociete.jsx
+++ resources/js/Pages/Associase/IndexAssociete.jsx
@@ -1,43 +1,14 @@
 import Footer from "@/Components/Footer";
 import NavBar from "@/Components/Home/NavBar/Navbar";
 import { Head } from "@inertiajs/react";
+import BannerDestak from '@/Components/Home/bannerDestak/bannerDestak';
 
 export default function Welcome() {
     return (
         <>
-            <Head title="Porquê Fortaleza?" />
-
+            <Head title="Associe-se" />
             <NavBar />
-
-            <h1 className="uppercase tracking-widest font-raleway font-black text-[#0C9C95] py-3 lg:text-4xl text-4xl pt-32 px-10">
-                Associe-se
-            </h1>
-
-            <p className="text-gray-600 text-justify lg:text-lg text-lg pb-5 px-12">
-                O Fortaleza CVB conta atualmente com XX associados dos mais
-                variados segmentos que compõem a cadeia do turismo na região. A
-                instituição é uma hub que conecta empresas da cadeia produtiva.
-                Hotéis, centros e fornecedores de eventos e viagens,
-                organizadores e promotores de congressos, convenções, feiras e
-                shows, operadores de turismo receptivo, bares e restaurantes,
-                comércio e empresas de transportes reunidas, trabalham de forma
-                cooperativada para o aumento do fluxo turístico de lazer, de
-                eventos e de negócios. Os associados desempenham um papel
-                fundamental e estratégico na qualidade de atendimento aos
-                turistas. Junte-se a nós e participe de treinamentos, rodadas de
-                negócios e eventos que fortalecem o turismo e os negócios da
-                região. Preencha o formulário e identifique o plano de anuidade
-                que corresponde ao seu tipo de estabelecimento, envie para que
-                nossa equipe entre em contato.
-                <br />
-                <br />
-                <span className="text-md text-red-600">
-                    Precisa de ajuda ou quer mais informações? Chame no
-                    XXXXXXXXX
-                </span>
-            </p>
-            <section className="">formulário</section>
-
+            <BannerDestak pagina="ASSOCIESE" posicao={1} />
             <Footer />
         </>
     );
