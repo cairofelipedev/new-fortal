276b912b1714d7677635d2cdf887645cac066e09
cairofelipedev <cairofelipedev@gmail.com>
2025-04-30 11:52:02 -0300

    Fix

--- resources/js/Pages/About/Index.jsx
+++ resources/js/Pages/About/Index.jsx
@@ -1,16 +1,8 @@
 import Footer from '@/Components/Footer';
 import BannerDestak from '@/Components/Home/bannerDestak/bannerDestak';
-import BannerHero from '@/Components/Home/bannerHero/bannerHero';
-import EnjoyFortaleza from '@/Components/Home/enjoyFortaleza/enjoyFortaleza';
-import Highlights from '@/Components/Home/highlights/highlights';
 import NavBar from '@/Components/Home/NavBar/Navbar';
-import NextEvents from '@/Components/CalendarEvents/nextEvents/nextEvents';
-import OtherFortal from '@/Components/Home/OtherFortal';
-import VideoArea from '@/Components/Home/videoArea/videoArea';
 import { Head } from '@inertiajs/react';
-import InitialExplain from '@/Components/About/initialAbout/InitialAbout';
-import Differencials from '@/Components/About/differencials/differencials';
-import HowToGet from '@/Components/About/howToGet/howToGet';
+import SimpleSlider from '@/Components/Banners/BannersHome';
 
 export default function Welcome() {
     return (
